a16536d9da0d9b3716b3b6f6742adc169846e2bd
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-20 18:55:42 -0300

    alterando atributos das tags html

--- js/functions.js
+++ js/functions.js
@@ -3,5 +3,14 @@ $(function () {
 });
 
 $(document).ready(() => {
-    console.log($('div').attr('style', 'background-color: blue'));
+    $('div').attr('style', 'background-color: blue; width: 200px; height: 200px');
+});
+$(document).ready(() => {
+    $('img').attr('src', 'img/esfera_2.png');
+    $('img').attr('border', '2');
+});
+
+$(document).ready(() => {
+    $('input').attr('value', 'Eu continuo sendo um input');
+    $('input').attr('type', 'password');
 });
